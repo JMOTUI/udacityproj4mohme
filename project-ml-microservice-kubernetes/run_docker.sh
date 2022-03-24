43670ed9dcc9fa1836add8fb5955905aafcb14c1
Jens Mohme <Jens.mohme@tui.com>
2022-03-24 12:06:13 +0000

    fixed errors in run docker file and ran a successful test

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,10 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-dock build --tag=mlapi .
+docker build --tag=mlapi .
 # Step 2: 
 # List docker images
 docker image ls
 # Step 3: 
 # Run flask app
-docker run -p 8080:80 mlapi
+docker run -p 8000:80 mlapi
